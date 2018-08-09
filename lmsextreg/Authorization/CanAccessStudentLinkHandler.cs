47ed64c8586fbf90be64af82d6d592174460a684
LeeTrent <Lee.Trent.1@gmail.com>
2018-08-09 10:30:10 -0500

    Makes sure that 2-factor authentication is enabled before showing links to secure resources

--- lmsextreg/Authorization/CanAccessStudentLinkHandler.cs
+++ lmsextreg/Authorization/CanAccessStudentLinkHandler.cs
@@ -9,62 +9,115 @@ namespace lmsextreg.Authorization
 {
     public class CanAccessStudentLinkHandler : AuthorizationHandler<CanAccessStudentLink>
     {
-        UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
 
-        public CanAccessStudentLinkHandler(UserManager<ApplicationUser> userMgr)
+        public CanAccessStudentLinkHandler(SignInManager<ApplicationUser> signInMgr)
         {
-            _userManager = userMgr;
+            _signInManager = signInMgr;
 
-            Console.WriteLine("[CanAccessStudentLinkHandler][Constructor] - (_userManager == null): " 
-                                + (_userManager == null));
+            Console.WriteLine("[CanAccessStudentLinkHandler][Constructor] - (_signInManager == null): " 
+                                + (_signInManager == null));
+
+            Console.WriteLine("[CanAccessStudentLinkHandler][Constructor] - (_signInManager.UserManager == null): " 
+                                + (_signInManager.UserManager == null));                                
         }        
 
-        protected override Task HandleRequirementAsync
-        (
-            AuthorizationHandlerContext authContext,
-            CanAccessStudentLink requirement
-        )
-        {
-           // Console.WriteLine("[][] - (): " + );
-
-            string userId = _userManager.GetUserId(authContext.User);
-            Console.WriteLine("[CanAccessStudentLinkHandler][HandleRequirementAsync] - "
-                                + "(ApplicationUser.Id): " 
-                                + userId
-                            );
-
-            ApplicationUser appUser = GetUserAsync(userId).Result;
-        
-            Console.WriteLine("[CanAccessStudentLinkHandler][HandleRequirementAsync] - (ApplicationUser == null): " 
-                                + (appUser == null));            
+        // protected override Task HandleRequirementAsync (AuthorizationHandlerContext authContext,
+        //                                                 CanAccessStudentLink requirement)
+        // {
+        // //     Console.WriteLine("[][] - (): " + );
+        //     Console.WriteLine("[CanAccessStudentLinkHandler][HandleRequirementAsync] - "
+        //                         + "(_signInManager.IsSignedIn(authContext.User): " 
+        //                         + _signInManager.IsSignedIn(authContext.User)
+        //                     );
             
-            Console.WriteLine("[CanAccessStudentLinkHandler][HandleRequirementAsync] - "
-                                + "(ApplicationUser.TwoFactorEnabled): " 
-                                + appUser.TwoFactorEnabled
-                            );
+        //     string userId = _signInManager.UserManager.GetUserId(authContext.User);
+        //     Console.WriteLine("[CanAccessStudentLinkHandler][HandleRequirementAsync] - "
+        //                         + "(ApplicationUser.Id): " 
+        //                         + userId
+        //                     );
+        //     ApplicationUser appUser = GetUserAsync(userId).Result;
+        //     Console.WriteLine   ("[CanAccessStudentLinkHandler][HandleRequirementAsync] - (ApplicationUser == null): " 
+        //                             + (appUser == null)
+        //                         );                      
+        //     Console.WriteLine   ("[CanAccessStudentLinkHandler][HandleRequirementAsync] - "
+        //                             + "(ApplicationUser.TwoFactorEnabled): " 
+        //                             + appUser.TwoFactorEnabled
+        //                         );
+        //    Console.WriteLine("[CanAccessStudentLinkHandler][HandleRequirementAsync] - "
+        //                         + "(authContext.User.IsInRole(\"STUDENT\"): " 
+        //                         + authContext.User.IsInRole("STUDENT")
+        //                   );
 
-           Console.WriteLine("[CanAccessStudentLinkHandler][HandleRequirementAsync] - "
-                                + "(authContext.User.IsInRole(\"STUDENT\"): " 
-                                + authContext.User.IsInRole("STUDENT")
-                            );
+        //     //string          userId              = _signInManager.UserManager.GetUserId(authContext.User);
+        //     //ApplicationUser appUser             = GetUserAsync(userId).Result;
+        //     bool            isSignedIn          = _signInManager.IsSignedIn(authContext.User);
+        //     bool            isTwoFactorEnabled  = appUser.TwoFactorEnabled;
+        //     bool            isStudent           = authContext.User.IsInRole("STUDENT");
 
-            if ( appUser.TwoFactorEnabled )
+        //     Console.WriteLine("[CanAccessStudentLinkHandler][HandleRequirementAsync] - (User is signed-in)....: " + isSignedIn);
+        //     Console.WriteLine("[CanAccessStudentLinkHandler][HandleRequirementAsync] - (Two-Factor is enabled): " + isTwoFactorEnabled);
+        //     Console.WriteLine("[CanAccessStudentLinkHandler][HandleRequirementAsync] - (User is a Student)....: " + isStudent);
+
+        //     if ( isSignedIn 
+        //             && isTwoFactorEnabled
+        //             && isStudent )
+        //     {
+        //         authContext.Succeed(requirement);
+        //     }
+
+        //     return Task.CompletedTask;
+        // }
+
+        protected override Task HandleRequirementAsync (AuthorizationHandlerContext authContext,
+                                                        CanAccessStudentLink requirement)
+        {
+            bool isSignedIn         = false;
+            bool isTwoFactorEnabled = false;
+            bool isStudent          = false;
+
+            isSignedIn = _signInManager.IsSignedIn(authContext.User);
+            Console.WriteLine("[CanAccessStudentLinkHandler][HandleRequirementAsync] - (User is signed-in): " + isSignedIn);
+
+            if ( isSignedIn )
+            {
+                string appUserId = _signInManager.UserManager.GetUserId(authContext.User);
+                Console.WriteLine("[CanAccessStudentLinkHandler][HandleRequirementAsync] - (ApplicationUser.Id): " + appUserId);  
+
+                if ( String.IsNullOrEmpty(appUserId) == false )
+                {
+                    ApplicationUser appUser = GetUserAsync(appUserId).Result;
+                    Console.WriteLine("[CanAccessStudentLinkHandler][HandleRequirementAsync] - (ApplicationUser == null): "  + (appUser == null) );
+
+                    if ( appUser != null) 
+                    {
+                        isTwoFactorEnabled = appUser.TwoFactorEnabled;
+                    }
+
+                    if ( authContext != null && authContext.User != null)
+                    {
+                        isStudent = authContext.User.IsInRole("STUDENT");
+                    }
+                }
+            }
+       
+            Console.WriteLine("[CanAccessStudentLinkHandler][HandleRequirementAsync] - (isSignedInn)........: " + isSignedIn);
+            Console.WriteLine("[CanAccessStudentLinkHandler][HandleRequirementAsync] - (isTwoFactorEnabled).: " + isTwoFactorEnabled);
+            Console.WriteLine("[CanAccessStudentLinkHandler][HandleRequirementAsync] - (isStudent)..........: " + isStudent);
+
+            if ( isSignedIn 
+                    && isTwoFactorEnabled 
+                    && isStudent )
             {
                 authContext.Succeed(requirement);
-            }
-            
+            }            
 
-            // if (authContext.User.IsInRole("STUDENT"))
-            // {
-            //     authContext.Succeed(requirement);
-            // }
-
-            return Task.FromResult(0);
+            return Task.CompletedTask;
         }
 
         private  Task<ApplicationUser> GetUserAsync(string userId)
         {
-            return _userManager.FindByIdAsync(userId);
+            return _signInManager.UserManager.FindByIdAsync(userId);
         }
     }
 }
