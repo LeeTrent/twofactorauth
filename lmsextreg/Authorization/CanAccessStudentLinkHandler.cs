ebfd3c93bd65cb7c26567dff2f0bd7c3273bd80e
LeeTrent <Lee.Trent.1@gmail.com>
2018-08-09 11:54:28 -0500

    Removes debug statements

--- lmsextreg/Authorization/CanAccessStudentLinkHandler.cs
+++ lmsextreg/Authorization/CanAccessStudentLinkHandler.cs
@@ -14,60 +14,7 @@ namespace lmsextreg.Authorization
         public CanAccessStudentLinkHandler(SignInManager<ApplicationUser> signInMgr)
         {
             _signInManager = signInMgr;
-
-            Console.WriteLine("[CanAccessStudentLinkHandler][Constructor] - (_signInManager == null): " 
-                                + (_signInManager == null));
-
-            Console.WriteLine("[CanAccessStudentLinkHandler][Constructor] - (_signInManager.UserManager == null): " 
-                                + (_signInManager.UserManager == null));                                
         }        
-
-        // protected override Task HandleRequirementAsync (AuthorizationHandlerContext authContext,
-        //                                                 CanAccessStudentLink requirement)
-        // {
-        // //     Console.WriteLine("[][] - (): " + );
-        //     Console.WriteLine("[CanAccessStudentLinkHandler][HandleRequirementAsync] - "
-        //                         + "(_signInManager.IsSignedIn(authContext.User): " 
-        //                         + _signInManager.IsSignedIn(authContext.User)
-        //                     );
-            
-        //     string userId = _signInManager.UserManager.GetUserId(authContext.User);
-        //     Console.WriteLine("[CanAccessStudentLinkHandler][HandleRequirementAsync] - "
-        //                         + "(ApplicationUser.Id): " 
-        //                         + userId
-        //                     );
-        //     ApplicationUser appUser = GetUserAsync(userId).Result;
-        //     Console.WriteLine   ("[CanAccessStudentLinkHandler][HandleRequirementAsync] - (ApplicationUser == null): " 
-        //                             + (appUser == null)
-        //                         );                      
-        //     Console.WriteLine   ("[CanAccessStudentLinkHandler][HandleRequirementAsync] - "
-        //                             + "(ApplicationUser.TwoFactorEnabled): " 
-        //                             + appUser.TwoFactorEnabled
-        //                         );
-        //    Console.WriteLine("[CanAccessStudentLinkHandler][HandleRequirementAsync] - "
-        //                         + "(authContext.User.IsInRole(\"STUDENT\"): " 
-        //                         + authContext.User.IsInRole("STUDENT")
-        //                   );
-
-        //     //string          userId              = _signInManager.UserManager.GetUserId(authContext.User);
-        //     //ApplicationUser appUser             = GetUserAsync(userId).Result;
-        //     bool            isSignedIn          = _signInManager.IsSignedIn(authContext.User);
-        //     bool            isTwoFactorEnabled  = appUser.TwoFactorEnabled;
-        //     bool            isStudent           = authContext.User.IsInRole("STUDENT");
-
-        //     Console.WriteLine("[CanAccessStudentLinkHandler][HandleRequirementAsync] - (User is signed-in)....: " + isSignedIn);
-        //     Console.WriteLine("[CanAccessStudentLinkHandler][HandleRequirementAsync] - (Two-Factor is enabled): " + isTwoFactorEnabled);
-        //     Console.WriteLine("[CanAccessStudentLinkHandler][HandleRequirementAsync] - (User is a Student)....: " + isStudent);
-
-        //     if ( isSignedIn 
-        //             && isTwoFactorEnabled
-        //             && isStudent )
-        //     {
-        //         authContext.Succeed(requirement);
-        //     }
-
-        //     return Task.CompletedTask;
-        // }
 
         protected override Task HandleRequirementAsync (AuthorizationHandlerContext authContext,
                                                         CanAccessStudentLink requirement)
@@ -77,17 +24,14 @@ namespace lmsextreg.Authorization
             bool isStudent          = false;
 
             isSignedIn = _signInManager.IsSignedIn(authContext.User);
-            Console.WriteLine("[CanAccessStudentLinkHandler][HandleRequirementAsync] - (User is signed-in): " + isSignedIn);
 
             if ( isSignedIn )
             {
                 string appUserId = _signInManager.UserManager.GetUserId(authContext.User);
-                Console.WriteLine("[CanAccessStudentLinkHandler][HandleRequirementAsync] - (ApplicationUser.Id): " + appUserId);  
 
                 if ( String.IsNullOrEmpty(appUserId) == false )
                 {
                     ApplicationUser appUser = GetUserAsync(appUserId).Result;
-                    Console.WriteLine("[CanAccessStudentLinkHandler][HandleRequirementAsync] - (ApplicationUser == null): "  + (appUser == null) );
 
                     if ( appUser != null) 
                     {
